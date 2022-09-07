6bade5d126424002a3008d1e0f870181aaa0306a
BlissedOut <4154540+RazorCoding@users.noreply.github.com>
2022-09-07 15:42:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,75 @@
 #include<cstdlib>
 
 bool GameOver = false;
-char input;
-int Move=0;
-int T=0;
+char Player1,Player2;
+int Move=0 , i=0;
 int main() // Main functions begins
-{	
-	std::cout << "\t\t\t\tTicTacToe" << std::endl;
-	while(GameOver == false)
+{
+	std::cout << "\t\t\t\t\tTicTacToe" << std::endl;
+	std::cout << "1. Double player" << std::endl;
+	std::cout << "2. Single player" << std::endl;
+	std::cin >> i;
+	switch(i)
 	{
-		DrawBorad();
-		std::cout << "[x or o]: ";
-		std::cin >> input;
-		std::cout << "1 to 9" << std::endl;
-		std::cout << ": ";
-		std::cin >> Move;
+		case 1:
+			std::cout << "Chosse: x/o" << std::endl;
+			std::cout << "Player 1: ";
+			std::cin  >> Player1;
+			std::cout << "Player 2: ";
+			std::cin  >> Player2;
+			if(Player1 == 'x' && Player2 == 'o')
+			{
+				std::cout << "Player 1: " << Player1 << "Player 2: " << Player2 << std::endl;
+			}else 
+			{
+				std::cerr  << "Choose x/o" << std::endl;
+			}
 
-		Insert(input,Move);
-		T=Check(Move);
-		std::cout << "Move Left: " << T << std::endl;
-		if(Win())
+		case 2:
+			std::cout << "Setting up Computer function" << std::endl;
+			Player1 = 'x'; // Setting player1 variable to x
+			Player2 = 'o'; // Setting Player2 varibale to o
+
+	} 
+	while(!GameOver)
+	{
+		if(i == 1)
 		{
-			std::cout << "WON!" << std::endl;
-			GameOver = true;
+			DrawBorad();
+			std::cout << "Player 1 Turn" << std::endl;
+			std::cout << "Move: 1-9 " << std::endl <<  ": " << std::endl;
+			std::cin >> Move;
+			Insert(Player1,Move);
+			DrawBorad();
+			if(Win(Player1))
+			{
+				std::cout << "Player 1 is the winner. " << Player1 << std::endl;
+			}
+
+			std::cout << "Player 2 Turn " << std::endl;
+			std::cout << "Move: 1-9" << std::endl <<  ": " << std::endl;
+			std::cin >> Move;
+			Insert(Player2,Move);
+			DrawBorad();
+			if(Win(Player2))
+			{
+				std::cout << "Player 2 is the winner. " << Player2  << std::endl;
+			}
+		}else if(i == 2)
+		{
+			while(!GameOver)
+			{
+				DrawBorad();
+				std::cout << "Your Move: 1-9" << std::endl;
+				std::cout << " : ";
+				std::cin >> Move;
+				Insert(Player1,Move);
+				std::cout << std::endl;
+				std::cout << "Computer Move: " << std::endl;
+				ComputerMove(Player2);
+				DrawBorad();
+			}
 		}
 	}
-	system("pause");
-	return 0; // returns 0 -- successo
-}
+		return 0; // returns 0 -- successo
+	}
