3569e88f3f455c130cf51a6f5e10edeba76bbab3
BlissedOut <4154540+RazorCoding@users.noreply.github.com>
2022-09-28 12:22:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,77 +1,62 @@
 #include<iostream>
-#include "functions.h"
 #include<cstdlib>
 
+// Custom libs
+#include "functions.h"
+
 bool GameOver = false;
-char Player1,Player2;
-int Move=0 , i=0;
+const char P1='x',P2='o';
+int Move=0 , Selected=0, MF=0;
 int main() // Main functions begins
 {
-	std::cout << "\t\t\t\t\tTicTacToe" << std::endl;
-	std::cout << "1. Double player" << std::endl;
-	std::cout << "2. Single player" << std::endl;
-	std::cin >> i;
-	switch(i)
+
+	banner();
+	std::cout << "1. Single Player" << std::endl;
+	std::cout << "2. Double Player" << std::endl;
+	std::cout  << "Select Number: ";
+	std::cin  >> Selected;
+	std::cout << std::endl;
+	switch(Selected)
 	{
 		case 1:
-			std::cout << "Chosse: x/o" << std::endl;
-			std::cout << "Player 1: ";
-			std::cin  >> Player1;
-			std::cout << "Player 2: ";
-			std::cin  >> Player2;
-			if(Player1 == 'x' && Player2 == 'o')
-			{
-				std::cout << "Player 1: " << Player1 << "Player 2: " << Player2 << std::endl;
-			}else 
-			{
-				std::cerr  << "Choose x/o" << std::endl;
-			}
 
-		case 2:
-			std::cout << "Setting up Computer function" << std::endl;
-			Player1 = 'x'; // Setting player1 variable to x
-			Player2 = 'o'; // Setting Player2 varibale to o
-
-	} 
-	while(!GameOver)
-	{
-		if(i == 1)
-		{
-			DrawBorad();
-			std::cout << "Player 1 Turn" << std::endl;
-			std::cout << "Move: 1-9 " << std::endl <<  ": " << std::endl;
-			std::cin >> Move;
-			Insert(Player1,Move);
-			DrawBorad();
-			if(Win(Player1))
-			{
-				std::cout << "Player 1 is the winner. " << Player1 << std::endl;
-			}
-
-			std::cout << "Player 2 Turn " << std::endl;
-			std::cout << "Move: 1-9" << std::endl <<  ": " << std::endl;
-			std::cin >> Move;
-			Insert(Player2,Move);
-			DrawBorad();
-			if(Win(Player2))
-			{
-				std::cout << "Player 2 is the winner. " << Player2  << std::endl;
-			}
-		}else if(i == 2)
-		{
-			while(!GameOver)
-			{
+			std::cout << "P1 is 'x'" << std::endl << "P2 is 'o'" << std::endl;
+			while(!GameOver){
 				DrawBorad();
-				std::cout << "Your Move: 1-9" << std::endl;
-				std::cout << " : ";
+				std::cout << "P1 Move: ";
+				std::cin  >> Move;
+				MoveLeft(Move);
+					if(check(Move,P1)){ 				
+						InsertMove(P1,Move);
+						DrawBorad();
+						
+						if(Win(P1))
+							{
+								GameOver=true;
+								break;
+							}
+					}
+				
+				
+				Move = 0; // Reseting Value of Move Variable					
+				
+				std::cout << "P2 Move: ";
 				std::cin >> Move;
-				Insert(Player1,Move);
-				std::cout << std::endl;
-				std::cout << "Computer Move: " << std::endl;
-				ComputerMove(Player2);
-				DrawBorad();
+				MoveLeft(Move);
+
+				if(check(Move,P2))
+				{
+					InsertMove(P2,Move);
+					DrawBorad();
+					MoveLeft(Move);
+					if(Win(P2))
+					{
+							GameOver = true;
+							break;
+					}
+				}				
 			}
-		}
 	}
-		return 0; // returns 0 -- successo
-	}
+system("pause");
+	return 0; // returns 0 -- success
+}
